70e32ba879d9cdda19c0a9a65fbab9376ae140aa
Derrick Korir <derrickkip@Derricks-MacBook-Pro.local>
2019-03-09 16:45:06 +0300

    chore(setup): Setup development environment for react
    - configure webpack
    - add babel

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div className="full-screen">
+      <div>
+        <h1>
+          React Page {" "}
+        </h1>
+        <br />
+        <a
+          className="button-line"
+          href="https://github.com/derrickkip"
+          target="_blank"
+        >
+          Know more
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i, 
+        loader: "file-loader?name=/public/icons/[name].[ext]"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "public", "index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
